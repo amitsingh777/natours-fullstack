db3c0ed0c5fb7b7d574b10b0ef0696e2e20cc8fb
Amit Singh <amitbrajeshsingh@gmail.com>
2024-04-09 15:39:56 +0530

    Completed post api creation for creating a new tour

--- app.js
+++ app.js
@@ -5,15 +5,33 @@ const app = express();
 const listeningPortNumber = 8000;
 const ipAddress = '127.0.0.1';
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
+const toursFilePath = `${__dirname}/dev-data/data/tours-simple.json`;
+const tours = JSON.parse(fs.readFileSync(toursFilePath));
+
+app.use(express.json());
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).send({
     status: 'success',
     results: tours.length,
     tours,
+  });
+});
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  const latestTours = [...tours, newTour];
+  fs.writeFile(toursFilePath, JSON.stringify(latestTours), (error) => {
+    if (error) {
+      res.status(500).json({
+        status: 'fail',
+        message: 'Internal Server error',
+      });
+    }
+    res.status(201).json({
+      status: 'success',
+      data: newTour,
+    });
   });
 });
 
