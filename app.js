a9bd2747667d5c3f0e922463d8cb6c0b9028eb9c
Amit Singh <amitbrajeshsingh@gmail.com>
2024-04-09 14:39:47 +0530

    Added tours list api

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 const listeningPortNumber = 8000;
 const ipAddress = '127.0.0.1';
-app.get('/', (req, res) => {
-  res.status(200).send('Here are your resources');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).send({
+    status: 'success',
+    results: tours.length,
+    tours,
+  });
 });
+
 app.listen(listeningPortNumber, ipAddress, () => {
   console.log(
     `Listening at ${'http://' + ipAddress + ':' + listeningPortNumber}`
